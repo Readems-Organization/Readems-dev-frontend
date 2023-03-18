fb843b3fcee7792be753a879f45448b2cbdbfdaf
Timothy Attah <timothyattah733@gmail.com>
2023-03-18 14:26:40 +0100

    Initial commits and setups

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+
+const App: FC = (): ReactElement => {
+  return (
+    <div>
+      <h1>This is readems homepage</h1>
+    </div>
+  );
+};
+
+export default App;
